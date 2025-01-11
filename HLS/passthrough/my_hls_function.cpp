f4d3d2f0187471cb7bda730d45fa30f06a984c4e
PALanga23 <palanga@wpi.edu>
2025-01-10 20:56:51 -0500

    8 bit pixel passthrough

---
+++ HLS/passthrough/my_hls_function.cpp
@@ -0,0 +1,25 @@
+#include "my_hls_function.h"
+
+// HLS function to read from input stream and write to output stream
+void my_hls_function(hls::stream<axis_t> &in_stream, hls::stream<axis_t> &out_stream) {
+    #pragma HLS INTERFACE axis port=in_stream
+    #pragma HLS INTERFACE axis port=out_stream
+    #pragma HLS INTERFACE ap_ctrl_none port=return
+
+    // Process each input element
+    while (!in_stream.empty()) {
+        #pragma HLS PIPELINE II=1
+
+        // Read data from input stream
+        axis_t input_data = in_stream.read();
+
+        axis_t output_data;
+        output_data.data = input_data.data;
+        output_data.last = 1;
+        output_data.keep = 1;
+        output_data.strb = 1;
+
+        // Write data to output stream
+        out_stream.write(output_data);
+    }
+}
---
+++ HLS/passthrough/my_hls_function.h
@@ -0,0 +1,18 @@
+#ifndef MY_HLS_FUNCTION_H
+#define MY_HLS_FUNCTION_H
+
+#include <hls_stream.h>
+#include <ap_int.h>
+#include <ap_axi_sdata.h>
+
+// Define pixel_t as an 8-bit unsigned integer
+typedef ap_uint<8> pixel_t; // 8-bit per channel for each pixel
+
+typedef ap_uint<32> bytes_streamed;
+
+// Define axis_t with data width of 8 bits and no additional signals
+typedef hls::axis<bytes_streamed, 0, 0, 0> axis_t;
+
+void my_hls_function(hls::stream<axis_t> &in_stream, hls::stream<axis_t> &out_stream);
+
+#endif
