27529c7fae3b9764f3baf7243d0f479e8f2620c6
jwbuchta_mqp_vm <jwbuchta@mqp_vm.wpi.edu>
2025-01-22 16:33:08 +0000

    Move petalinux here the hard way

--- petalinux/kv260_plnx_proj/project-spec/meta-user/recipes-apps/interpolate2x/files/interpolate2x.cpp
+++ petalinux/kv260_plnx_proj/project-spec/meta-user/recipes-apps/interpolate2x/files/interpolate2x.cpp
@@ -15,7 +15,7 @@ This thing is going to be so ungodly slow, but it is just a prototype. The main 
 Steps 2, 4, 5, and 6 will all be deleted eventually
 
 Author: Will Buchta
-Date Modified: 12/26/2024
+Date Modified: 1/11/2025
 
 */
 
@@ -41,7 +41,6 @@ Date Modified: 12/26/2024
 #include "PhysMem.h"
 
 // Must define either DMA_DIRECT_REG_MODE or DMA_SG_MODE before including axi-dma.h
-#define DMA_DIRECT_REG_MODE 1
 #include "axi-dma.h"
 
 #include "argparse.hpp"
