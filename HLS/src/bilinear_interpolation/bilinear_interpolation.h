6bd8485ef7ad8043dd9c2c98c2c6d44533174fd0
PALanga23 <palanga@wpi.edu>
2025-02-07 16:42:40 -0500

    updates to load and unload properly in test bench

--- HLS/src/bilinear_interpolation/bilinear_interpolation.h
+++ HLS/src/bilinear_interpolation/bilinear_interpolation.h
@@ -17,11 +17,13 @@
 #define BITS_PER_PIXEL 8
 #define NUM_TRANSFERS (WIDTH_IN*HEIGHT_IN*CHANNELS*BITS_PER_PIXEL/BITS_PER_TRANSFER)
 #define NUM_TRANSFERS_OUT (NUM_TRANSFERS*SCALE_FACTOR*SCALE_FACTOR)
+#define PIXELS_PER_TRANSFERS (BITS_PER_TRANSFER / BITS_PER_PIXEL)
 
 
 // Define pixel_t as an 8-bit unsigned integer
 typedef ap_uint<8> pixel_t; // 8-bit per channel for each pixel
 typedef ap_uint<32> data_streamed;
+typedef ap_uint<128> temp_streamed;
 
 // Define axis_t with data width of 8 bits and no additional signals
 typedef hls::axis<pixel_t, 0, 0, 0> axis_t;
--- HLS/src/bilinear_interpolation/bilinear_interpolation_tb.cpp
+++ HLS/src/bilinear_interpolation/bilinear_interpolation_tb.cpp
@@ -20,6 +20,49 @@ int main() {
     	test_data[i] = temp;
     }
 
+    temp_streamed loaded[147];
+
+    for(int load = 0; load < 147; load++){
+    	int upper_range = 0;
+    	int lower_range = 0;
+    	temp_streamed temp_load;
+
+    	for(int transfer_pixel = 0; transfer_pixel < 16; transfer_pixel++){
+    		upper_range = transfer_pixel * 8 + 7;
+    		lower_range = transfer_pixel * 8;
+    		temp_load.range(upper_range, lower_range) = coin_tile_low_res[load * 16 + transfer_pixel];
+    	}
+
+    	loaded[load] = temp_load;
+    }
+
+    for(int unload = 0; unload < 147; unload++){
+    	int upper_range = 0;
+    	int lower_range = 0;
+    	uint8_t temp_pixel;
+
+    	for(int transfer_pixel = 0; transfer_pixel < 16; transfer_pixel++){
+    		upper_range = transfer_pixel * 8 + 7;
+    		lower_range = transfer_pixel * 8;
+    		temp_pixel = loaded[unload].range(upper_range, lower_range);
+
+
+			if ((uint8_t)temp_pixel != coin_tile_low_res[unload * 16 + transfer_pixel]) {
+				std::cout << "ERROR: Load Mismatch at index " << (unload * 16 + transfer_pixel)
+						  << " (expected " << (int)coin_tile_low_res[unload * 16 + transfer_pixel]
+						  << ", got " << (int)temp_pixel << ")\n";
+			}
+			else {
+				std::cout << "SUCCESS: Load Match at index " << (unload * 16 + transfer_pixel)
+						  << " (expected " << coin_tile_low_res[unload * 16 + transfer_pixel]
+						  << ", got " << (int)temp_pixel << ")\n";
+			}
+    	}
+
+
+    }
+
+
     test_data[NUM_TRANSFERS] = (pixel_t)0;
 
 	// Fill the input stream with test data
@@ -35,7 +78,7 @@ int main() {
 	}
 
 
-    // Call the HLS function
+    //Instantiate DUT
     bilinear_interpolation(in_stream, out_stream);
 
     // Check the output stream
@@ -52,6 +95,8 @@ int main() {
 			axis_t output_element = out_stream.read();
 			pixel_t data = output_element.data;
 
+
+			/*
 			// Verify the data matches
 			if ((uint8_t)data != coin_tile_interpolated[j]) {
 				std::cout << "ERROR: Mismatch at index " << j
@@ -64,7 +109,7 @@ int main() {
 						  << " (expected " << coin_tile_interpolated[j]
 						  << ", got " << data << ")\n";
 				success = true;
-			}
+			} */
 
 			if(output_element.keep != 0b1 || output_element.strb != 0b1){
 				success = false;
