db444da8b8f207da64a7dd83ffe43f6f30458678
PALanga23 <palanga@wpi.edu>
2025-03-01 17:23:10 -0500

    adds buffer to split sections

--- HLS/src/bilinear_interpolation/bilinear_interpolation.cpp
+++ HLS/src/bilinear_interpolation/bilinear_interpolation.cpp
@@ -6,9 +6,8 @@
 int bilinear_interpolation_calculations(pixel_t image_in[SLIDER_HEIGHT_IN][SLIDER_WIDTH_IN],
                                         pixel_t image_out[SLIDER_HEIGHT_OUT][SLIDER_WIDTH_OUT]) {
 
-
-    fixed widthRatio  = fixed(SLIDER_WIDTH_WO_BUFFER - 1) / fixed(SLIDER_WIDTH_OUT - 1);
-    fixed heightRatio = fixed(SLIDER_HEIGHT_WO_BUFFER - 1) / fixed(SLIDER_HEIGHT_OUT - 1);
+    fixed widthRatio  = fixed(SLIDER_WIDTH_IN - 1) / fixed(SLIDER_WIDTH_OUT - 1);
+    fixed heightRatio = fixed(SLIDER_HEIGHT_IN - 1) / fixed(SLIDER_HEIGHT_OUT - 1);
 
     for (int y_out = 0; y_out < SLIDER_HEIGHT_OUT; ++y_out) {
 
@@ -19,14 +18,14 @@ int bilinear_interpolation_calculations(pixel_t image_in[SLIDER_HEIGHT_IN][SLIDE
             #pragma HLS UNROLL factor=2
 
             // Compute the corresponding input coordinates in fixed point
-            fixed x_in = x_out * widthRatio + 1;  // Offset by 1 to account for buffer
-            fixed y_in = y_out * heightRatio + 1; // Offset by 1 to account for buffer
+            fixed x_in = x_out * widthRatio;
+            fixed y_in = y_out * heightRatio;
 
             // Determine the four nearest neighbors
             int x0 = static_cast<int>(x_in);
             int y0 = static_cast<int>(y_in);
-            int x1 = std::min(x0 + 1, SLIDER_WIDTH_WO_BUFFER - 1);
-            int y1 = std::min(y0 + 1, SLIDER_HEIGHT_WO_BUFFER - 1);
+            int x1 = std::min(x0 + 1, SLIDER_WIDTH_IN - 1);
+            int y1 = std::min(y0 + 1, SLIDER_HEIGHT_IN - 1);
 
             // Calculate interpolation weights
             fixed dx = x_in - fixed(x0);
@@ -68,12 +67,11 @@ int bilinear_interpolation_calculations(pixel_t image_in[SLIDER_HEIGHT_IN][SLIDE
 }
 
 // Function to stream input samples into 2D array
-//void stream_samples_in(hls::stream<axis_t> &in_stream, pixel_t input_data_stored[HEIGHT_IN][WIDTH_IN]) {
-void stream_samples_in(hls::stream<axis_t> &in_stream, hls::stream<data_streamed, PIXELS_PER_TRANSFER> &data_fifo_in){
+void stream_samples_in(hls::stream<axis_t> &in_stream, pixel_t input_data_stored[HEIGHT_IN][WIDTH_IN]) {
 
     int i = 0;
 
-    //data_streamed input_streams_stored[NUM_TRANSFERS];
+    data_streamed input_streams_stored[NUM_TRANSFERS];
 
     while (i < NUM_TRANSFERS) {
 
@@ -84,142 +82,29 @@ void stream_samples_in(hls::stream<axis_t> &in_stream, hls::stream<data_streamed
             }
 
             axis_t temp_input = in_stream.read();
-            data_streamed temp_data = temp_input.data;
-            data_fifo_in.write(temp_data);
+            input_streams_stored[i] = temp_input.data;
 
             i++;
         }
     }
 
-}
+    for (int transfer = 0; transfer < NUM_TRANSFERS; transfer++) {
 
-void fill_image_fifos(hls::stream<data_streamed, PIXELS_PER_TRANSFER> &input_stream,
-					  hls::stream<pixel_t, WIDTH_IN> &storage_fifo_overlap_top,
-					  hls::stream<pixel_t, WIDTH_IN> &storage_fifo_0,
-					  hls::stream<pixel_t, WIDTH_IN> &storage_fifo_1,
-					  hls::stream<pixel_t, WIDTH_IN> &storage_fifo_2,
-					  hls::stream<pixel_t, WIDTH_IN> &storage_fifo_3,
-					  hls::stream<pixel_t, WIDTH_IN> &storage_fifo_4,
-					  hls::stream<pixel_t, WIDTH_IN> &storage_fifo_overlap_bottom) {
+        for (int j = 0; j < PIXELS_PER_TRANSFER; j++) {
 
-
-    int count = 0;  // Keeps track of how many values have been written to the current FIFO
-    int fifo_index = 0;  // Tracks which FIFO is currently being filled
-    bool first_fill = true;
-
-    while (!input_stream.empty()) {
-
-        data_streamed temp_data = input_stream.read();  // Read from input stream
-        pixel_t data_split[PIXELS_PER_TRANSFER];
-
-        //Handle filling the top overlap stream either with 0s or values used to be on the bottom
-        if (fifo_index == 0) {
-
-        	for(int i = 0; i < PIXELS_PER_TRANSFER; i++){
-				if (first_fill) {
-					// Fill storage_fifo_overlap_top with 0s on the first fill of fifo_0
-					storage_fifo_overlap_top.write((pixel_t)0);
-
-					//if top row is fully filled with zeros have next write fill from bottom buffer
-					if(i == PIXELS_PER_TRANSFER - 1){
-						first_fill = false;
-					}
-
-				} else {
-					// Fill storage_fifo_overlap_top with values from storage_fifo_overlap_bottom
-					if (!storage_fifo_overlap_bottom.empty()) {
-						storage_fifo_overlap_top.write(storage_fifo_overlap_bottom.read());
-					}
-				}
-        	}
-        }
-
-        //split input stream into pixels
-        for (int pixel_transfer = 0; pixel_transfer < PIXELS_PER_TRANSFER; pixel_transfer++) {
-        	data_split[pixel_transfer] = temp_data.range(pixel_transfer * BITS_PER_PIXEL + 23, pixel_transfer * BITS_PER_PIXEL);
-        }
-
-
-        // Write data to the appropriate FIFO
-        switch (fifo_index) {
-            case 0:
-
-                if (first_fill) {
-                    // Fill storage_fifo_overlap_top with 0s on the first fill of fifo_0
-                    storage_fifo_overlap_top.write((pixel_t)0);
-                    first_fill = false;
-                } else {
-                    // Fill storage_fifo_overlap_top with values from storage_fifo_overlap_bottom
-                    if (!storage_fifo_overlap_bottom.empty()) {
-                        storage_fifo_overlap_top.write(storage_fifo_overlap_bottom.read());
-                    }
-                }
-
-                for(int i = 0; i < PIXELS_PER_TRANSFER; i++){
-                	storage_fifo_0.write(data_split[i]);
-                }
-
-            	break;
-            case 1:
-
-                for(int i = 0; i < PIXELS_PER_TRANSFER; i++){
-                	storage_fifo_1.write(data_split[i]);
-                }
-
-            	break;
-            case 2:
-
-                for(int i = 0; i < PIXELS_PER_TRANSFER; i++){
-                	storage_fifo_2.write(data_split[i]);
-                }
-
-            	break;
-            case 3:
-
-                for(int i = 0; i < PIXELS_PER_TRANSFER; i++){
-                	storage_fifo_3.write(data_split[i]);
-                }
-
-            	break;
-            case 4:
-
-                for(int i = 0; i < PIXELS_PER_TRANSFER; i++){
-                	storage_fifo_4.write(data_split[i]);
-                	storage_fifo_overlap_bottom.write(data_split[i]);
-                }
-
-                break;
-        }
-
-        count++;
-
-        // Switch to the next FIFO after 7 writes
-        if (count == 7) {
-            count = 0;
-            fifo_index = (fifo_index + 1) % 5;  // Cycle back to FIFO 0 after FIFO 4
+            int index = transfer * PIXELS_PER_TRANSFER + j;
+            int y = index / WIDTH_IN;
+            int x = index % WIDTH_IN;
+            input_data_stored[y][x] = input_streams_stored[transfer].range(j * BITS_PER_PIXEL + 23, j * BITS_PER_PIXEL);
         }
     }
 }
-
 
 // Function to stream output samples from 2D array
-//void stream_samples_out(pixel_t output_data_stored[HEIGHT_OUT][WIDTH_OUT], hls::stream<axis_t> &out_stream) {
-void stream_samples_out(hls::stream<data_streamed, PIXELS_PER_TRANSFER> &data_fifo_out, hls::stream<axis_t> &out_stream){
-    //data_streamed loaded[NUM_TRANSFERS_OUT];
+void stream_samples_out(pixel_t output_data_stored[HEIGHT_OUT][WIDTH_OUT], hls::stream<axis_t> &out_stream) {
+    data_streamed loaded[NUM_TRANSFERS_OUT];
 
-    for (int i = 0; i < NUM_TRANSFERS_OUT; i++) {
-        axis_t output_stream;
-        output_stream.data = data_fifo_out.read();
-        output_stream.last = (i == NUM_TRANSFERS_OUT - 1);
-        output_stream.keep = 0xFFFF;
-        output_stream.strb = 0xFFFF;
-        out_stream.write(output_stream);
-    }
-}
-
-void fill_output_fifo(pixel_t output_data_stored[HEIGHT_OUT][WIDTH_OUT], hls::stream<data_streamed, PIXELS_PER_TRANSFER> &output_fifo){
-
-	for (int load = 0; load < NUM_TRANSFERS_OUT; load++) {
+    for (int load = 0; load < NUM_TRANSFERS_OUT; load++) {
 
         data_streamed temp_load = 0;
 
@@ -231,12 +116,28 @@ void fill_output_fifo(pixel_t output_data_stored[HEIGHT_OUT][WIDTH_OUT], hls::st
             temp_load.range(pixel_transfer * BITS_PER_PIXEL + 23, pixel_transfer * BITS_PER_PIXEL) = output_data_stored[y][x];
         }
 
-        output_fifo.write(temp_load);
+        loaded[load] = temp_load;
+    }
+
+    for (int i = 0; i < NUM_TRANSFERS_OUT; i++) {
+        axis_t output_stream;
+        output_stream.data = loaded[i];
+        output_stream.last = (i == NUM_TRANSFERS_OUT - 1);
+        output_stream.keep = 0xFFFF;
+        output_stream.strb = 0xFFFF;
+        out_stream.write(output_stream);
     }
 }
 
+void assemble_slider(pixel_t input_data_stored[HEIGHT_IN][WIDTH_IN], pixel_t input_data_slider[SLIDER_HEIGHT_IN][SLIDER_WIDTH_IN], int width_start, int height_start){
 
 
+}
+
+void disassemble_slider(pixel_t output_data_slider[SLIDER_HEIGHT_OUT][SLIDER_WIDTH_OUT], pixel_t output_data_stored[HEIGHT_OUT][WIDTH_OUT], int width_start, int height_start){
+
+
+}
 
 // Main function for bilinear interpolation processing
 void bilinear_interpolation(hls::stream<axis_t> &in_stream, hls::stream<axis_t> &out_stream) {
@@ -244,127 +145,87 @@ void bilinear_interpolation(hls::stream<axis_t> &in_stream, hls::stream<axis_t> 
     #pragma HLS INTERFACE axis port=out_stream
     #pragma HLS INTERFACE ap_ctrl_none port=return
 
-	hls::stream<data_streamed, PIXELS_PER_TRANSFER> input_fifo;
-	//#pragma HLS BIND_STORAGE variable=input_fifo  type=ram_2p impl=bram
-	hls::stream<data_streamed, PIXELS_PER_TRANSFER> output_fifo;
-	//#pragma HLS BIND_STORAGE variable=output_fifo type=ram_2p impl=bram
-
-	hls::stream<pixel_t, WIDTH_IN> storage_fifo_0;
-	hls::stream<pixel_t, WIDTH_IN> storage_fifo_1;
-	hls::stream<pixel_t, WIDTH_IN> storage_fifo_2;
-	hls::stream<pixel_t, WIDTH_IN> storage_fifo_3;
-	hls::stream<pixel_t, WIDTH_IN> storage_fifo_4;
-
-	hls::stream<pixel_t, WIDTH_IN> storage_fifo_overlap_top;
-	hls::stream<pixel_t, WIDTH_IN> storage_fifo_overlap_bottom;
-
+	//partition input and output data, still in BRAM -> partitioning array will allow you to write ot multiple mem locations at same time
+	//decrease latency
+    pixel_t input_data_stored[HEIGHT_IN][WIDTH_IN];
+    #pragma HLS BIND_STORAGE variable=input_data_stored type=RAM_2P impl=BRAM
+	#pragma HLS array_partition variable=input_data_stored type=complete factor=4
 
     pixel_t output_data_stored[HEIGHT_OUT][WIDTH_OUT];
-    //#pragma HLS BIND_STORAGE variable=output_data_stored type=RAM_2P impl=BRAM
+    #pragma HLS BIND_STORAGE variable=output_data_stored type=RAM_2P impl=BRAM
+	#pragma HLS array_partition variable=output_data_stored type=complete factor=4
 
 
-    //#pragma HLS DATAFLOW
-    stream_samples_in(in_stream, input_fifo);
-    fill_image_fifos(input_fifo, storage_fifo_overlap_top,
-    				 storage_fifo_0, storage_fifo_1, storage_fifo_2,
-    				 storage_fifo_3, storage_fifo_4, storage_fifo_overlap_bottom);
+    #pragma HLS DATAFLOW
+    stream_samples_in(in_stream, input_data_stored);
 
+    for(int i = 0; i < NUM_SLIDERS_HEIGHT; i++){
 
-    //local caches for storing values on edges used more than once
-    pixel_t edge_row_0 = 0, edge_row_1 = 0, edge_row_2 = 0, edge_row_3 = 0, edge_row_4 = 0, edge_row_5 = 0;
+    	for(int j = 0; j < NUM_SLIDERS_WIDTH; j++){
 
+    		//these should be LUT ram
+    		pixel_t temp_data_slider_in[SLIDER_HEIGHT_IN][SLIDER_WIDTH_IN];
+			#pragma HLS array_partition variable=temp_data_slider_in type=complete factor=4
+    		pixel_t temp_data_slider_out[SLIDER_HEIGHT_OUT][SLIDER_WIDTH_OUT];
+			#pragma HLS array_partition variable=temp_data_slider_out type=complete factor=4
 
-    int i = 0, j = 0;
+    		//int height_start_in = i * SLIDER_HEIGHT_IN;
+    		//int width_start_in = j * SLIDER_WIDTH_IN;
+    		int height_start_out = i * SLIDER_HEIGHT_OUT;
+    		int width_start_out = j * SLIDER_WIDTH_OUT;
 
-    //SPLIT IMAGE INTO SLIDING ARRAY
-    while(i < NUM_SLIDERS_HEIGHT){
+    		/*for(int x = 0; x < SLIDER_HEIGHT_IN; x++){
+    			for(int y = 0; y < SLIDER_WIDTH_IN; y++){
+					#pragma HLS unroll factor=SLIDER_WIDTH_IN
+    				temp_data_slider_in[x][y] = input_data_stored[height_start_in + x][width_start_in + y];
+    				//int data_to_store = input_data_stored[height_start + i][width_start + j];
+    			}
+    		}*/
 
-		if(!storage_fifo_0.empty() && !storage_fifo_1.empty() && !storage_fifo_2.empty() &&
-		   !storage_fifo_3.empty() && !storage_fifo_4.empty()){
+    		int height_start_in = -1;
+    		int width_start_in = -1;
 
-			while(j < NUM_SLIDERS_WIDTH){
+    		//loop through sliding window w/ buffer size
+    		for(int row = 0; row < SLIDER_BUFFER_HEIGHT; row++){
+    			for(int col = 0; col < SLIDER_BUFFER_HEIGHT; col++){
 
-				//SWITCH OVER TO FIFOS
+    				//calculate the position of the pixel being pulled from the image to store
+    				int row_pos = height_start_in + row;
+    				int col_pos = width_start_in + col;
+    				pixel_t temp_pixel;
 
-				//these should be LUT ram
-				pixel_t temp_data_slider_in[SLIDER_HEIGHT_IN][SLIDER_WIDTH_IN];
-				//#pragma HLS array_partition variable=temp_data_slider_in type=complete factor=4
-				pixel_t temp_data_slider_out[SLIDER_HEIGHT_OUT][SLIDER_WIDTH_OUT];
-				//#pragma HLS array_partition variable=temp_data_slider_out type=complete factor=4
+    				//if index is outside the bounds of the image
+    				if(row_pos < 0 || row_pos >= HEIGHT_IN || col_pos < 0 ||col_pos >= WIDTH_IN){
+    					temp_pixel = (pixel_t)0;
+    				}
+    				else{
+    					temp_pixel = input_data_stored[row_pos][col_pos];
+    				}
 
+    				temp_data_slider_in[row][col] = temp_pixel;
+    			}
+    		}
 
+    		width_start_in += SLIDER_WIDTH_IN;
+    		if(width_start_in >= WIDTH_IN){
+    			width_start_in = -1;
+    			height_start_in += SLIDER_HEIGHT_IN;
+    		}
 
-				//will need to manually update if we want to change sliders height
-				//handles overlap on left and right
-				for(int col = 0; col < SLIDER_WIDTH_IN; col++){
+    		bilinear_interpolation_calculations(temp_data_slider_in, temp_data_slider_out);
 
-					//handle image overlap to the left
-					if(col == 0){
-						temp_data_slider_in[0][col] = edge_row_0;
-						temp_data_slider_in[1][col] = edge_row_1;
-						temp_data_slider_in[2][col] = edge_row_2;
-						temp_data_slider_in[3][col] = edge_row_3;
-						temp_data_slider_in[4][col] = edge_row_4;
-						temp_data_slider_in[5][col] = edge_row_5;
-					}
-					else{
-						pixel_t temp_0 = storage_fifo_overlap_top.read();
-						pixel_t temp_1 = storage_fifo_0.read();
-						pixel_t temp_2 = storage_fifo_1.read();
-						pixel_t temp_3 = storage_fifo_2.read();
-						pixel_t temp_4 = storage_fifo_3.read();
-						pixel_t temp_5 = storage_fifo_4.read();
-
-						//if not in the overlap on the right
-						if(col < SLIDER_WIDTH_IN - 2){
-							temp_data_slider_in[0][col] = temp_0;
-							temp_data_slider_in[1][col] = temp_1;
-							temp_data_slider_in[2][col] = temp_2;
-							temp_data_slider_in[3][col] = temp_3;
-							temp_data_slider_in[4][col] = temp_4;
-							temp_data_slider_in[5][col] = temp_5;
-						}
-						//handle image overlap on the right
-						else{
-							temp_data_slider_in[0][col] = temp_0;
-							temp_data_slider_in[1][col] = temp_1;
-							temp_data_slider_in[2][col] = temp_2;
-							temp_data_slider_in[3][col] = temp_3;
-							temp_data_slider_in[4][col] = temp_4;
-							temp_data_slider_in[5][col] = temp_5;
-
-							edge_row_0 = temp_0;
-							edge_row_1 = temp_1;
-							edge_row_2 = temp_2;
-							edge_row_3 = temp_3;
-							edge_row_4 = temp_4;
-							edge_row_5 = temp_5;
-						}
-					}
-				}
-
-				bilinear_interpolation_calculations(temp_data_slider_in, temp_data_slider_out);
-
-				int height_start_out = i * SLIDER_HEIGHT_OUT;
-				int width_start_out = j * SLIDER_WIDTH_OUT;
-
-				//try moving this step into bilinear_interpolation_calculations so that once the value is calculated
-				//its stored directly in output_data_stored instead of an output data slider array
-				for(int row = 0; row < SLIDER_HEIGHT_OUT; row++){
-					for(int col = 0; col < SLIDER_WIDTH_OUT; col++){
-					//#pragma HLS unroll factor=SLIDER_WIDTH_OUT
-						output_data_stored[height_start_out + row][width_start_out + col] = temp_data_slider_out[row][col];
-					}
-				}
-
-				j++;
-
-			}
-
-			i++;
-		}
+    		//try moving this step into bilinear_interpolation_calculations so that once the value is calculated
+    		//its stored directly in output_data_stored instead of an output data slider array
+    		for(int x = 0; x < SLIDER_HEIGHT_OUT; x++){
+    			for(int y = 0; y < SLIDER_WIDTH_OUT; y++){
+				#pragma HLS unroll factor=SLIDER_WIDTH_OUT
+    				output_data_stored[height_start_out + x][width_start_out + y] = temp_data_slider_out[x][y];
+    				//int data_to_store = output_data_slider[i][j];
+    			}
+    		}
+    	}
     }
 
-    fill_output_fifo(output_data_stored, output_fifo);
-    stream_samples_out(output_fifo, out_stream);
+
+    stream_samples_out(output_data_stored, out_stream);
 }
--- HLS/src/bilinear_interpolation/bilinear_interpolation_tb.cpp
+++ HLS/src/bilinear_interpolation/bilinear_interpolation_tb.cpp
@@ -168,6 +168,75 @@ int main() {
 
 	}
 
+    /*int j = 0;
+
+	while(j < 588){
+
+		while(!out_stream.empty()){
+
+			// Read the output stream
+			axis_t output_element = out_stream.read();
+			data_streamed data = output_element.data;
+
+			int upper_range = 0;
+			int lower_range = 0;
+			channel_t temp_pixel;
+
+			for(int transfer_pixel = 0; transfer_pixel < 16; transfer_pixel++){
+				upper_range = transfer_pixel * 8 + 7;
+				lower_range = transfer_pixel * 8;
+				temp_pixel = data.range(upper_range, lower_range);
+
+				// Verify the data matches
+				if ((uint8_t)temp_pixel >= (coin_tile_interpolated[j * 16 + transfer_pixel] + 4)){
+					std::cout << "ERROR: Mismatch at index " << (j * 16 + transfer_pixel)
+							  << " (expected " << (int)coin_tile_interpolated[j * 16 + transfer_pixel]
+							  << ", got " << (int)temp_pixel << ")\n";
+					success = false;
+				}
+				else if ((uint8_t)temp_pixel <= (coin_tile_interpolated[j * 16 + transfer_pixel] - 4)){
+									std::cout << "ERROR: Mismatch at index " << (j * 16 + transfer_pixel)
+											  << " (expected " << (int)coin_tile_interpolated[j * 16 + transfer_pixel]
+											  << ", got " << (int)temp_pixel << ")\n";
+									success = false;
+				}
+				else {
+					std::cout << "SUCCESS: Match at index " << (j * 16 + transfer_pixel)
+							  << " (expected " << coin_tile_interpolated[j * 16 + transfer_pixel]
+							  << ", got " << (int)temp_pixel << ")\n";
+					//success = true;
+				}
+
+			}
+
+
+
+			if(output_element.keep != 0xFFFF || output_element.strb != 0xFFFF){
+				success = false;
+				std:: cout << "keep or strb wrong";
+				break;
+			}
+
+
+
+			if((j < (NUM_TRANSFERS_OUT - 1)) && output_element.last == true){
+				success = false;
+				std::cout << "last triggered before end\n";
+				break;
+			}
+
+			else if(j == (NUM_TRANSFERS_OUT - 1) && output_element.last == false){
+				success = false;
+				std::cout << "last not triggered at end\n";
+				break;
+			}
+
+			j++;
+
+		}
+
+    } */
+
 
     // Final result
     if (success) {
