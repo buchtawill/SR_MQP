c6419f2ab3f363ce087c251d28761f29c30deac7
buchtawill <buchtawill@gmail.com>
2025-03-04 17:09:08 -0500

    Finish conv ideal (yay)

--- Models/FSRCNN/conv_ideal.py
+++ Models/FSRCNN/conv_ideal.py
@@ -37,6 +37,8 @@ def get_real_conv_result(start_x, start_y, channel_arr):
     return result
 
 def fifo_psum_conv(channel_arr):
+    
+    ############################## Do the first 4 rows ##############################
     # print("First row slider")
     for i in range(28):
         slider = channel_arr[0, i:i+5]
@@ -68,18 +70,33 @@ def fifo_psum_conv(channel_arr):
         psums[3].append(np.sum(slider * WEIGHTS[0,3]) + psums[2].pop(0))
         
     # # print("\nFifth row slider / start printing results")
+    ############################## Pipeline is ready ##############################
     outputs = np.zeros((28,28))
-    for row in range(4, 28):
+    for row in range(4, 32):
         for i in range(28):
             slider = channel_arr[row, i:i+5]
             # print(slider)
-            psums[0].append(np.sum(slider * WEIGHTS[0,0]))
-            psums[1].append(np.sum(slider * WEIGHTS[0,1]) + psums[0].pop(0))
-            psums[2].append(np.sum(slider * WEIGHTS[0,2]) + psums[1].pop(0))
-            psums[3].append(np.sum(slider * WEIGHTS[0,3]) + psums[2].pop(0))
+            if(row < 28):
+                psums[0].append(np.sum(slider * WEIGHTS[0,0]))
+            if(row < 29):
+                psums[1].append(np.sum(slider * WEIGHTS[0,1]) + psums[0].pop(0))
+            if(row < 30):
+                psums[2].append(np.sum(slider * WEIGHTS[0,2]) + psums[1].pop(0))
+            if(row < 31):
+                psums[3].append(np.sum(slider * WEIGHTS[0,3]) + psums[2].pop(0))
             pre_activation = np.sum(slider * WEIGHTS[0,4]) + psums[3].pop(0) + BIAS_1
             output = np.maximum(0, pre_activation) + PRELU_1 * np.minimum(0, pre_activation)
             outputs[row-4, i] = output
+    print(len(psums[0]))
+    print(len(psums[1]))
+    print(len(psums[2]))
+    print(len(psums[3]))
+    
+    ############################ Drain the pipeline ############################
+    # for i in range(28):
+    #     slider = channel_arr[28, i:i+5]
+        # print(slider)
+        
     
     return outputs
 
