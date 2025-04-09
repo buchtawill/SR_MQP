9d25d6162f034c4eb7eb2ff5e8f83f273cc65915
jwbuchta_mqp_vm <jwbuchta@mqp_vm.wpi.edu>
2025-04-09 05:38:45 +0000

    Update build to have different scripts for zcu102 and kv260 build

--- vivado/edit_proj_kv260.sh
+++ vivado/edit_proj_kv260.sh
@@ -9,11 +9,11 @@ if [ -d "kv260_vivado_project" ]; then
     if [ "$choice" = "overwrite" ]; then
         rm -rf kv260_vivado_project
         echo "Overwriting existing project"
-        vivado -mode batch -source create_project.tcl
+        vivado -mode batch -source create_project_kv260.tcl
     fi
 # the project directory does not exist
 else 
-    vivado -mode batch -source create_project.tcl
+    vivado -mode batch -source create_project_kv260.tcl
 fi
 
 if [ $? -ne 0 ]; then
@@ -21,4 +21,4 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-vivado -mode batch -source start_gui.tcl
+vivado -mode tcl -source start_gui_kv.tcl
---
+++ vivado/start_gui_zcu.tcl
@@ -0,0 +1,4 @@
+# todo: add command line arg handling
+open_project zcu102_vivado_project/zcu102_vivado_project.xpr
+
+start_gui
