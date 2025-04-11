737f493a5f59329e4d422d3ce35ed2bff2c45ac0
buchtawill <buchtawill@gmail.com>
2025-04-11 13:52:46 -0400

    Update batch scripts

--- vivado/edit_project_kv260.bat
+++ vivado/edit_project_kv260.bat
@@ -5,14 +5,14 @@ REM This script will either open the project or recreate it if it already exists
 call "C:\Xilinx\Vivado\2023.1\settings64.bat"
 
 if exist "kv260_vivado_project" (
-    set /p choice="The directory 'kv260_vivado_project' already exists. Do you want to remove it? (y/n): "
-    if /i "%choice%"=="y" (
+    set /p choice="The directory 'kv260_vivado_project' already exists. Do you want to remove it? (overwrite/n): "
+    if /i "%choice%"=="overwrite" (
         rmdir /s /q kv260_vivado_project
         echo Overwriting existing project
-        vivado -mode batch -source create_project.tcl
+        vivado -mode batch -source create_project_kv260.tcl
     )
 ) else (
-    vivado -mode batch -source create_project.tcl
+    vivado -mode batch -source create_project_kv260.tcl
 )
 
 if %errorlevel% neq 0 (
@@ -20,4 +20,4 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-vivado -mode batch -source start_gui.tcl
+vivado -mode batch -source start_gui_kv.tcl
